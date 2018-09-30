2ed6d8f7a17ba04542c3c0fd3ac56456f9e93e06
alessio_diluzio <alessio.diluzio94@gmail.com>
2018-09-30 12:07:42 +0200

    rimozione classe inutilizzata

--- src/main/java/com/sdcc_project/service_interface/CloudLetService.java
+++
@@ -1,9 +0,0 @@
-package com.sdcc_project.service_interface;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface CloudLetService extends Remote {
-
-    void publishUpdate(String filename, String data, int version) throws RemoteException;
-}
